307a68026798999c05dcfa1c528e60cc08ebbd0e
RaccoonCode96 <waawoo13@gmail.com>
2021-09-19 02:15:56 +0900

    Update: 그룹단어 체커 test 완료

--- test.js
+++ test.js
@@ -4,37 +4,29 @@ let start = new Date();
 const fs = require('fs');
 const filePath = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
 let input = fs.readFileSync(filePath).toString().trim().split('\n');
-input = input[0];
 let count = 0;
-const ctAlphabet = ['c=', 'c-', 'd-', 'lj', 'nj', 's=', 'z='];
 
-function check2(input) {
-	if (!input) {
-		console.log(count);
-		return;
-	}
-	const cur = input.slice(0, 2);
-	if (ctAlphabet.indexOf(cur) !== -1) {
-		count += 1;
-		return check2(input.slice(2));
-	} else if (cur === 'dz') {
-		return check3(input.slice(0));
-	} else {
-		count += 1;
-		return check2(input.slice(1));
-	}
-}
+for (let i = 1; i < input.length; i++) {
+	const rowSet = new Set([]);
+	const row = input[i].trim().split('');
+	let isGroup = true;
 
-function check3(input) {
-	if (input.slice(0, 3) === 'dz=') {
+	for (let i = 0; i < row.length; i++) {
+		const cur = row[i];
+		const prev = i === 0 ? 0 : row[i - 1];
+		if (cur !== prev && rowSet.has(cur)) {
+			isGroup = false;
+			break;
+		}
+		rowSet.add(cur);
+	}
+
+	if (isGroup) {
 		count += 1;
-		return check2(input.slice(3));
-	} else {
-		return check2(input);
 	}
 }
 
-check2(input);
+console.log(count);
 
 // ----------------- check time -----------------
 let end = new Date();
