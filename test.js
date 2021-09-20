349253302e79af0da20bcbacd7bf6a11cdb96493
RaccoonCode96 <waawoo13@gmail.com>
2021-09-20 23:45:51 +0900

    Update: 부녀회장 문제 test

--- test.js
+++ test.js
@@ -1,32 +1,31 @@
 let start = new Date();
 // ----------------- code -----------------
-
 const fs = require('fs');
 const filePath = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
 let input = fs.readFileSync(filePath).toString().trim().split('\n');
-let count = 0;
-
-for (let i = 1; i < input.length; i++) {
-	const rowSet = new Set([]);
-	const row = input[i].trim().split('');
-	let isGroup = true;
-
-	for (let i = 0; i < row.length; i++) {
-		const cur = row[i];
-		const prev = i === 0 ? 0 : row[i - 1];
-		if (cur !== prev && rowSet.has(cur)) {
-			isGroup = false;
-			break;
-		}
-		rowSet.add(cur);
+const testCount = Number(input[0]);
+const rooms = Array.from({ length: 15 }, () => Array(14).fill(0));
+let answer = '';
+for (let i = 0; i < 15; i++) {
+	rooms[i][0] = 1;
+	if (i < 14) {
+		rooms[0][i] = i + 1;
 	}
-
-	if (isGroup) {
-		count += 1;
+}
+for (let i = 1; i < 14; i++) {
+	for (let j = 1; j < 15; j++) {
+		rooms[j][i] = rooms[j - 1][i] + rooms[j][i - 1];
 	}
 }
 
-console.log(count);
+for (let i = 0; i < testCount; i++) {
+	const floor = Number(input[2 * i + 1]);
+	const line = Number(input[2 * i + 2]) - 1;
+	answer += `${rooms[floor][line]}\n`;
+}
+
+console.log(answer);
+// j 층, i 호
 
 // ----------------- check time -----------------
 let end = new Date();
